d5d48e58f79e612ff92309a1dc234e64051d8ebe
Michael Tighe <striderIIDX@gmail.com>
2013-11-06 13:50:32 -0600

    Moving bot & scraper

---
+++ salt_scraper.go
@@ -0,0 +1,338 @@
+package main
+
+/*
+	Data scraper for IRC bot; reads fight results from illuminati stat pages &
+	stores them in a database.
+	TODO:
+*/
+
+import (
+	"errors"
+	"fmt"
+	"github.com/moovweb/gokogiri"
+	ghtml "github.com/moovweb/gokogiri/html"
+	"github.com/moovweb/gokogiri/xml"
+	"html"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"os"
+	"regexp"
+	"spicerack"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Settings struct {
+	DbUser, DbPass, DbName string
+	IllumEmail, IllumPword string
+	RecentTournamentCount  int
+}
+
+type ParsedMatch struct {
+	Red, Blue, Winner          string
+	RedBets, BlueBets, Bettors int
+	MatchId                    int
+	FightWinner                spicerack.FightWinner
+}
+
+var (
+	repo  *spicerack.Repository
+	numRx *regexp.Regexp
+)
+
+func main() {
+	// load the config file
+	conf, err := spicerack.GofigFromEnv("ME_CONF")
+	if err != nil {
+		fmt.Printf("%v\nQuitting.\n", err)
+		os.Exit(1)
+	}
+
+	// inflate settings struct & open db connection
+	settings := &Settings{}
+	conf.Struct("salty", settings)
+	repo, err = spicerack.OpenDb(settings.DbUser, settings.DbPass, settings.DbName)
+	if err != nil {
+		fmt.Printf("Failed to connect to postgres: %v\n", err)
+		os.Exit(1)
+	}
+	defer repo.Close()
+
+	// set the cookie jar for consecutive requests after logging in
+	jar, _ := cookiejar.New(nil)
+	client := &http.Client{Jar: jar}
+	fmt.Println("Logging into saltybet")
+	r, _ := client.PostForm(saltyUrl("authenticate?signin=1"), url.Values{
+		"email":        {settings.IllumEmail},
+		"pword":        {settings.IllumPword},
+		"authenticate": {"signin"},
+	})
+
+	// validate the login succeeded
+	page, _ := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	if !validLogin(page) {
+		fmt.Println("Saltybet credentials were invalid.\nQuitting.\n")
+		os.Exit(1)
+	}
+
+	// compile a number regex, we'll be using it a lot in parsing
+	numRx, _ = regexp.Compile(`[0-9]+`)
+
+	// scrape the compendium for updated/new characters
+	fmt.Println("Scraping Roster")
+	if err := getRoster(client); err != nil {
+		fmt.Printf("Failed to scrape roster: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Get the last n number of tournaments & scrape 'em
+	count := settings.RecentTournamentCount
+	fmt.Printf("Grabbing last %d tournament Ids\n", count)
+	if ids, err := getLatestTournamentIds(client, count); err == nil {
+		for _, tournyId := range ids {
+			pageNum := 1
+			for {
+				fmt.Printf("Processing Tournament #%d, Page #%d\n", tournyId, pageNum)
+				hasNextPage, err := processTournament(client, tournyId, pageNum)
+				if err != nil {
+					fmt.Printf("Failed to parse tournament page: %v\n", err)
+					break
+				}
+				if !hasNextPage {
+					break
+				}
+				fmt.Println()
+				pageNum++
+			}
+			fmt.Println()
+		}
+		relayToBot(fmt.Sprintf("Nightly scrape complete, bot information is up to date."))
+	} else {
+		fmt.Printf("Failed to grab tournament IDs: %v\n", err)
+	}
+}
+
+// returns an absolute salty url based on a fragment
+func saltyUrl(format string, args ...interface{}) string {
+	rel := fmt.Sprintf(format, args...)
+	return fmt.Sprintf("http://www.saltybet.com/%s", strings.TrimPrefix(rel, "/"))
+}
+
+// validates salty login attempt
+func validLogin(page []byte) bool {
+	doc, _ := gokogiri.ParseHtml(page)
+	errors, _ := doc.Search("//div[@class='ui-state-error']")
+	return len(errors) == 0
+}
+
+// checks to ensure we have data to scrape
+func illuminatiCheck(rows []xml.Node) (err error) {
+	if len(rows) == 0 {
+		err = errors.New("unable to find tournaments/fight records, has your illuminati subscription run out?")
+	}
+	return
+}
+
+// runs through the tier pages, adding/updating characters accordingly
+func getRoster(c *http.Client) error {
+	// 1=S-Tier 2=A-Tier 3=B-Tier 4=Potato
+	for i := 1; i <= 4; i++ {
+		fmt.Printf("- Scraping Tier %d\n", i)
+		doc, err := getGokogiriDoc(c, saltyUrl("compendium?tier=%d", i))
+		if err != nil {
+			return err
+		}
+		rows, _ := doc.Search("//ul[@id='tierlist']/li/a")
+		for _, r := range rows {
+			cid, _ := strconv.Atoi(numRx.FindAllString(r.Attribute("href").String(), 2)[1])
+			name := html.UnescapeString(r.FirstChild().String())
+			fighter, _ := repo.GetFighter(cid)
+
+			if !fighter.InDb() {
+				fighter.CharacterId = cid
+			}
+			fighter.Name = name
+			fighter.Tier = i
+			if err := repo.UpdateFighter(fighter); err != nil {
+				fmt.Printf("Failed to update fighter #%d - '%s': %v\n", cid, name, err)
+			}
+		}
+	}
+	return nil
+}
+
+// For an entire re-scrape, this will be all the valid tournament ids to scrape.
+func getAllTournamentIds() ([]int, error) {
+	return []int{
+		/*46, 50, 52, 54, 55, 56, 59, 63, 65, 67, */
+		68, 70, 72, 73, 74, 75, 76, 77, 78,
+		79, 80, 81, 82, 83, 84, 85, 86, 87,
+		88, 89, 90, 91, 92, 93, 94, 95, 96,
+		97, 98, 99, 100, 101}, nil
+}
+
+// Returns an array of the ids of the last n tournaments.
+func getLatestTournamentIds(c *http.Client, count int) ([]int, error) {
+	doc, err := getGokogiriDoc(c, saltyUrl("stats?tournamentstats=1"))
+	if err != nil {
+		return nil, err
+	}
+
+	rows, _ := doc.Search(fmt.Sprintf("//table/tbody/tr[position() <= %d]", count))
+	if err = illuminatiCheck(rows); err != nil {
+		return nil, err
+	}
+
+	result := make([]int, count)
+	for i, r := range rows {
+		cols, _ := r.Search("td")
+		id, _ := strconv.Atoi(numRx.FindString(cols[0].FirstChild().Attribute("href").String()))
+		result[i] = id
+	}
+	return result, nil
+}
+
+// Runs through a tournament page, adding matches & updating fighter information
+func processTournament(c *http.Client, id, pageNum int) (bool, error) {
+	doc, err := getGokogiriDoc(c, saltyUrl("stats?tournament_id=%d&page=%d", id, pageNum))
+	if err != nil {
+		return false, err
+	}
+
+	rows, _ := doc.Search("//table/tbody/tr")
+	if err = illuminatiCheck(rows); err != nil {
+		return false, err
+	}
+	nextpage, _ := doc.Search("//div[@id='pagination']//a[text()='Next']")
+
+	scrapeRows(rows)
+	return len(nextpage) > 0, nil
+}
+
+// Returns a gokogiri html.Document from a url
+func getGokogiriDoc(c *http.Client, url string) (*ghtml.HtmlDocument, error) {
+	resp, err := c.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	page, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return gokogiri.ParseHtml(page)
+}
+
+// Scrape a match row, parsing information & storing it
+func scrapeRows(rows []xml.Node) {
+	skipped, updated := 0, 0
+	for _, r := range rows {
+		pm, err := GetParsedMatch(r)
+		if err != nil {
+			fmt.Printf("Error parsing match id #%d: %v\n", pm.MatchId, err)
+			continue
+		}
+
+		if !repo.MatchExists(pm.MatchId) {
+			red_fighter, _ := repo.GetFighter(pm.Red)
+			blue_fighter, _ := repo.GetFighter(pm.Blue)
+			red_fighter.TotalBets += pm.RedBets
+			blue_fighter.TotalBets += pm.BlueBets
+			spicerack.UpdateFighterElo(red_fighter, blue_fighter, pm.FightWinner)
+
+			if tx, e := repo.StartTransaction(); e == nil {
+				if rErr := repo.UpdateFighterInTrans(red_fighter, tx); rErr != nil {
+					fmt.Printf("--Skipping match, failed to update fighter: %v\n", rErr)
+					tx.Rollback()
+				} else if bErr := repo.UpdateFighterInTrans(blue_fighter, tx); bErr != nil {
+					fmt.Printf("--Skipping match, failed to update fighter: %v\n", bErr)
+					tx.Rollback()
+				} else {
+					m := &spicerack.Match{
+						MatchId: pm.MatchId,
+						RedId:   red_fighter.Id, BlueId: blue_fighter.Id,
+						RedBets: pm.RedBets, BlueBets: pm.BlueBets,
+						BetCount: pm.Bettors, Winner: int(pm.FightWinner),
+						Created: time.Now(), Updated: time.Now()}
+
+					if mErr := repo.InsertMatch(m); mErr == nil {
+						tx.Commit()
+						updated++
+					} else {
+						tx.Rollback()
+						fmt.Printf("--Failed to insert match #%d: %v\n", pm.MatchId, mErr)
+					}
+				}
+			}
+		} else {
+			skipped++
+		}
+	}
+	fmt.Printf("--Skipped: %d | New Matches: %d\n", skipped, updated)
+}
+
+// Parse a match row into a managed object
+func GetParsedMatch(n xml.Node) (pm *ParsedMatch, err error) {
+	pm = &ParsedMatch{}
+	match_url, _ := n.Search("td/a/@href")
+	red, _ := n.Search("td/a/span[@class='redtext']/text()")
+	redvalue, _ := n.Search("td/a/span[@class='redtext']/following-sibling::text()")
+	blue, _ := n.Search("td/a/span[@class='bluetext']/text()")
+	bluevalue, _ := n.Search("td/a/span[@class='bluetext']/following-sibling::text()")
+	winner, _ := n.Search("td[position() = 2]/span/text()")
+	bettors, _ := n.Search("td[last()]/text()")
+
+	if len(match_url) > 0 {
+		pm.MatchId, _ = strconv.Atoi(numRx.FindString(match_url[0].String()))
+	}
+	if len(redvalue) > 0 {
+		pm.RedBets, _ = strconv.Atoi(numRx.FindString(redvalue[0].String()))
+	}
+	if len(bluevalue) > 0 {
+		pm.BlueBets, _ = strconv.Atoi(numRx.FindString(bluevalue[0].String()))
+	}
+
+	pm.Red = html.UnescapeString(red[0].String())
+	pm.Blue = html.UnescapeString(blue[0].String())
+	pm.Bettors, _ = strconv.Atoi(bettors[0].String())
+	if len(winner) > 0 {
+		pm.Winner = html.UnescapeString(winner[0].String())
+		if pm.Winner == pm.Red {
+			pm.FightWinner = spicerack.WINNER_RED
+		} else if pm.Winner == pm.Blue {
+			pm.FightWinner = spicerack.WINNER_BLUE
+		}
+	}
+
+	if pm.MatchId == 0 {
+		err = errors.New("Unable to parse match id.")
+	} else if len(pm.Red) == 0 || len(pm.Blue) == 0 {
+		err = errors.New("Red or Blue fighter is an empty string.")
+	} else if int(pm.FightWinner) == 0 {
+		err = errors.New("No winner found.")
+	}
+
+	return pm, err
+}
+
+// Sends messages to the shaker bot, if listening on this machine at port 4380
+func relayToBot(msg string) {
+	form := url.Values{
+		"Message": {msg},
+	}
+
+	host, _ := os.Hostname()
+	port := 4380
+	url := fmt.Sprintf("http://%s:%d/shaker/bot/talk", host, port)
+	if resp, err := http.PostForm(url, form); err != nil {
+		fmt.Printf("Failed to send message to IRC bot: %v\n", err)
+	} else {
+		defer resp.Body.Close()
+	}
+}
---
+++ salt_shaker.go
@@ -0,0 +1,464 @@
+package main
+
+/*
+	IRC bot; reports current fight card w/ stats & hightower link in irc channel
+	Commands:
+		`s 		     - Reports the current fight card
+		`s  p1 (,p2) - Reports a specific fight card for p1 and/or p2
+		`sr p1 (,p2) - Reports a specific fight card for p1 and/or p2 for retired fighters
+		`r			 - [Admin] Registers the bot with NickServ
+		`c [token]	 - [Admin] Sends a registration confirmation token to NickServ
+	TODO:
+*/
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/oguzbilgic/socketio"
+	"io/ioutil"
+	"irc"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"spicerack"
+	"strings"
+	"syscall"
+	"time"
+)
+
+const (
+	MESSAGE_ENDPOINT string = "/shaker/bot/talk"
+	UNKNOWN_FIGHTER  string = "\x02\x0300New Challenger!\x03\x02"
+	BOT_ADMIN        string = "Lone_Strider"
+	// string formats
+	LOG_TIME_FORMAT      string = "2006-01-02 15:04:05"
+	P1_NAME_FORMAT       string = "\x02\x0304%s\x03\x02"
+	P2_NAME_FORMAT       string = "\x02\x0310%s\x03\x02"
+	REMATCH_FORMAT       string = "Rematch! %s has beaten %s!"
+	REMATCH_TRADE_FORMAT string = "Rematch! %s and %s have beaten each other!"
+	WINNER_FORMAT        string = "%s has defeated %s!"
+	UPSET_WINNER_FORMAT  string = "In a %s upset, %s has defeated %s!"
+	VS_FORMAT            string = "%s [%s] \x02vs\x02 %s [%s] | %s"
+	SOLO_FORMAT          string = "%s [%s] | %s"
+	HT_FORMAT            string = "http://fightmoney.herokuapp.com/stats/#/%s/%s"
+	TINYURL_FORMAT       string = "http://tinyurl.com/api-create.php?%s"
+
+	UPSET_FACTOR float64 = 2.0
+)
+
+type Settings struct {
+	DbName, DbUser, DbPass      string
+	Server, Channel, Nick, Pass string
+	BotEmail                    string
+	TheShiznit                  string
+	Websocket                   string
+	Pushover                    map[string]interface{}
+}
+
+type Options struct {
+	LooseSearch   bool
+	RetiredSearch bool
+}
+
+var (
+	settings     *Settings = &Settings{}
+	client       *irc.Client
+	lastAnnounce time.Time
+	shouldNotify bool        = true
+	logChannel   chan string = make(chan string)
+)
+
+func main() {
+	// thread-safe message logging
+	go listenForLogs()
+
+	// catching interrupt and terminate signals
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+	go catchSignals(sigs)
+
+	// loading from global config
+	log("PID: %d\n", os.Getpid())
+	log("Loading Configuration")
+	conf, err := spicerack.GofigFromEnv("ME_CONF")
+	if err != nil {
+		log("%v -  Quitting.", err)
+		os.Exit(1)
+	}
+
+	// reading from config & initialzing IRC client
+	conf.Struct("salty", settings)
+	settings.Pushover, _ = conf.Map("pushover")
+	client = irc.NewClient(settings.Server, settings.Nick, false)
+
+	// If the bot crashes, send a notification
+	defer func() {
+		if r := recover(); r != nil {
+			log("Panic! Sending notification: %v", r)
+			msg := fmt.Sprintf("%s is down! (%v)", settings.Nick, r)
+			notify(msg)
+		}
+	}()
+
+	// hookup irc command handling functions
+	client.HandleCommand(irc.RPL_WELCOME, registerAndJoin)
+	client.HandleCommand(irc.CMD_PRIVMSG, manualFightCard)
+	client.HandleCommand(irc.CMD_PRIVMSG, getSpecificFighters)
+	client.HandleCommand(irc.CMD_PRIVMSG, getRetiredFighters)
+	client.HandleCommand(irc.CMD_PRIVMSG, nickServ)
+
+	// connect to IRC & wait indefinitely, and listen for HTTP posts
+	// to relay to the channel
+	log("Connecting to IRC...")
+	client.Connect()
+	listenForRelays()
+	client.Wait()
+
+	if shouldNotify {
+		notify(fmt.Sprintf("%s has unexpectedly stopped!", settings.Nick))
+	}
+}
+
+// log messages for debugging
+func log(msg string, args ...interface{}) {
+	logChannel <- fmt.Sprintf("[%s] %s", time.Now().Format(LOG_TIME_FORMAT), fmt.Sprintf(msg, args...))
+}
+
+// go routine for logging messages to stdout
+func listenForLogs() {
+	for {
+		if msg, ok := <-logChannel; ok {
+			fmt.Println(strings.TrimRight(msg, "\n"))
+		} else {
+			break
+		}
+	}
+}
+
+// go routine waiting for an interrupt or terminate signal, so
+// the client can be closed gracefully
+func catchSignals(sigs <-chan os.Signal) {
+	for {
+		s := <-sigs
+		shouldNotify = false
+		log("Recieved OS Signal '%v', closing gracefully.", s)
+		client.Quit("Going down for an update, brb")
+		break
+	}
+}
+
+// when connected to the server, identify w/ nickserv, join CHANNEL and start polling salty
+func registerAndJoin(m *irc.Message) {
+	log("Connected to IRC, registering nick.")
+	client.Privmsg("NickServ", fmt.Sprintf("identify %s", settings.Pass))
+
+	log("Joining %s", settings.Channel)
+	client.Join(settings.Channel)
+
+	log("Starting websocket loop.")
+	go pollSalty()
+}
+
+// handles `s commands to get the current fight card
+func manualFightCard(m *irc.Message) {
+	if m.IsChannelMsg() && m.Parameters[0] == settings.Channel && m.Trail == "`s" {
+		if data, err := spicerack.GetSecretData(settings.TheShiznit); err == nil {
+			announceFightCard(data, nil)
+		}
+	}
+}
+
+// handles `s p1 [, p2]
+func getSpecificFighters(m *irc.Message) {
+	if m.IsChannelMsg() && m.Parameters[0] == settings.Channel && strings.HasPrefix(m.Trail, "`s ") {
+		data := createFightCard(m.Trail[3:])
+		opts := &Options{LooseSearch: true}
+		announceFightCard(data, opts)
+	}
+}
+
+// handles `sr p1 [, p2]
+func getRetiredFighters(m *irc.Message) {
+	if m.IsChannelMsg() && m.Parameters[0] == settings.Channel && strings.HasPrefix(m.Trail, "`sr ") {
+		data := createFightCard(m.Trail[4:])
+		opts := &Options{RetiredSearch: true}
+		announceFightCard(data, opts)
+	}
+}
+
+// generates a 'fake' fight card for the purposes of reporting specific requested fighters
+func createFightCard(str string) *spicerack.FightCard {
+	fighters := strings.Split(str, ",")
+	fc := &spicerack.FightCard{
+		RedName: strings.Trim(fighters[0], " "),
+	}
+
+	if len(fighters) > 1 {
+		fc.BlueName = strings.Trim(fighters[1], " ")
+	}
+
+	return fc
+}
+
+// NickServ registration/confirmation
+func nickServ(m *irc.Message) {
+	if m.Nick == BOT_ADMIN && strings.HasPrefix(m.Trail, "`") {
+		switch m.Trail[1:2] {
+		case "r":
+			client.Privmsg("NickServ", fmt.Sprintf("register %s %s", settings.Pass, settings.BotEmail))
+		case "c":
+			token := m.Trail[3:]
+			client.Privmsg("NickServ", fmt.Sprintf("confirm %s", token))
+		}
+	} else if !m.IsChannelMsg() {
+		// log any private messages the bot gets, why not?
+		log("<%s>: %s", m.Nick, m.Trail)
+	}
+}
+
+// websocket loop
+func pollSalty() {
+	for {
+		socket, err := socketio.DialAndConnect(settings.Websocket, "", "")
+		if err != nil {
+			log("Failed to connect to websocket: %v. Trying again in 10 sec.", err)
+			time.Sleep(time.Second * 10)
+			continue
+		}
+
+		var lastStatus string = ""
+		for {
+			_, err := socket.Receive()
+			if err != nil {
+				log("Failed to receive websocket data: %v. Reconnecting.", err)
+				break
+			}
+
+			data, err := spicerack.GetSecretData(settings.TheShiznit)
+			if err != nil {
+				log("%v", err)
+			} else {
+				if lastStatus != data.Status {
+					// reset the cooldown so we always get an up-to-date triggered annoucement
+					lastAnnounce = time.Now().Add(time.Second * -5)
+
+					if data.TakingBets() {
+						announceFightCard(data, nil)
+					} else if data.InProgress() {
+						announceOdds(data)
+					} else if data.WeHaveAWinner() {
+						announceWinner(data)
+					}
+
+					lastStatus = data.Status
+				}
+			}
+		}
+	}
+}
+
+// sends fight card / fighter stats information to IRC
+func announceFightCard(data *spicerack.FightCard, opts *Options) {
+	if len(data.RedName) == 0 && len(data.BlueName) == 0 {
+		return // Nothing to announce!
+	}
+
+	diff := time.Now().Sub(lastAnnounce)
+	if diff.Seconds() >= 3 {
+		db := spicerack.Db(settings.DbUser, settings.DbPass, settings.DbName)
+		var red, blue *spicerack.Fighter
+		var e error
+
+		if opts != nil && opts.LooseSearch {
+			red, blue, e = db.SearchFighters(data.RedName, data.BlueName)
+		} else if opts != nil && opts.RetiredSearch {
+			red, blue, e = db.SearchRetiredFighters(data.RedName, data.BlueName)
+		} else {
+			red, blue, e = db.GetFighters(data.RedName, data.BlueName)
+		}
+
+		if e == nil {
+			p1f := formatFighterName(red, data.RedName, P1_NAME_FORMAT)
+			p2f := formatFighterName(blue, data.BlueName, P2_NAME_FORMAT)
+			p1stats := formatFighterStats(red)
+			p2stats := formatFighterStats(blue)
+			ht := getHightowerUrl(data.RedName, data.BlueName)
+
+			var card string = ""
+			if len(data.RedName) > 0 && len(data.BlueName) > 0 {
+				card = fmt.Sprintf(VS_FORMAT, p1f, p1stats, p2f, p2stats, ht)
+			} else if len(data.RedName) > 0 {
+				card = fmt.Sprintf(SOLO_FORMAT, p1f, p1stats, ht)
+			} else if len(data.BlueName) > 0 {
+				card = fmt.Sprintf(SOLO_FORMAT, p2f, p2stats, ht)
+			}
+
+			client.Privmsg(settings.Channel, card)
+
+			state, err := db.GetRematchState(red, blue)
+			if err == nil {
+				switch state {
+				case spicerack.TradedWins:
+					client.Privmsg(settings.Channel, fmt.Sprintf(REMATCH_TRADE_FORMAT, p1f, p2f))
+				case spicerack.RedBeatBlue:
+					client.Privmsg(settings.Channel, fmt.Sprintf(REMATCH_FORMAT, p1f, p2f))
+				case spicerack.BlueBeatRed:
+					client.Privmsg(settings.Channel, fmt.Sprintf(REMATCH_FORMAT, p2f, p1f))
+				}
+			}
+
+			sprinkleMrsDash(data)
+		} else {
+			log("%v", e)
+		}
+		lastAnnounce = time.Now()
+	}
+}
+
+// sends odds to channel when available
+func announceOdds(data *spicerack.FightCard) {
+	p1 := formatFighterName(nil, data.RedName, P1_NAME_FORMAT)
+	p2 := formatFighterName(nil, data.BlueName, P2_NAME_FORMAT)
+	msg := fmt.Sprintf("%s %s %s", p1, data.Odds(), p2)
+	client.Privmsg(settings.Channel, msg)
+}
+
+// informs channel of the outcome of a match
+func announceWinner(data *spicerack.FightCard) {
+	p1 := formatFighterName(nil, data.RedName, P1_NAME_FORMAT)
+	p2 := formatFighterName(nil, data.BlueName, P2_NAME_FORMAT)
+	var w, l, msg string
+
+	if data.Winner() == data.RedName {
+		w = p1
+		l = p2
+	} else if data.Winner() == data.BlueName {
+		w = p2
+		l = p1
+	} else {
+		msg = "...I have no idea who won."
+	}
+
+	if data.Upset(UPSET_FACTOR) {
+		msg = fmt.Sprintf(UPSET_WINNER_FORMAT, data.Odds(), w, l)
+	} else {
+		msg = fmt.Sprintf(WINNER_FORMAT, w, l)
+	}
+
+	client.Privmsg(settings.Channel, msg)
+}
+
+// Gives irc formatting to a fighter name, or a fallback name if the fighter isn't in the db.
+func formatFighterName(f *spicerack.Fighter, fallback, format string) string {
+	if f != nil {
+		return fmt.Sprintf(format, f.Name)
+	} else {
+		return fmt.Sprintf(format, fallback)
+	}
+}
+
+// Get the stats for a fighter, or the unknown fighter message if the fighter is nil
+func formatFighterStats(f *spicerack.Fighter) string {
+	if f != nil {
+		return f.IrcStats()
+	}
+	return UNKNOWN_FIGHTER
+}
+
+// generates a tinyurl link to hightower for the given fighters
+func getHightowerUrl(left, right string) string {
+	link := fmt.Sprintf(HT_FORMAT, escapeName(left), escapeName(right))
+
+	v := url.Values{}
+	v.Set("url", link)
+
+	turl := fmt.Sprintf(TINYURL_FORMAT, v.Encode())
+	if resp, err := http.Get(turl); err == nil {
+		defer resp.Body.Close()
+		if body, err := ioutil.ReadAll(resp.Body); err == nil {
+			return string(body)
+		}
+	}
+
+	return "TinyURL sucks right now"
+}
+
+// escapes fighter names for hightower urls
+func escapeName(name string) string {
+	result := url.QueryEscape(name)
+	return strings.Replace(result, "+", "%20", -1)
+}
+
+// listen on an http endpoint for external messages to relay to the channel
+func listenForRelays() {
+	host, _ := os.Hostname()
+	port := 4380
+	addr := fmt.Sprintf("%s:%d", host, port)
+	http.HandleFunc(MESSAGE_ENDPOINT, handler)
+	log("Listening for message relays at http://%s%s", addr, MESSAGE_ENDPOINT)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log("%v", err)
+	}
+}
+
+// handle any hits to the http endpoint
+func handler(w http.ResponseWriter, r *http.Request) {
+	h := w.Header()
+	if r.Method == "POST" {
+		msg := r.FormValue("Message")
+		if len(msg) > 0 {
+			log("Message: %s", msg)
+			client.Privmsg(settings.Channel, msg)
+			h.Set("X-Success", "true")
+		} else {
+			h.Set("X-Success", "false")
+			h.Set("X-Error", "One or more expected values were missing or incorrect.")
+		}
+	} else {
+		w.WriteHeader(401)
+	}
+}
+
+// sends pushover notifications
+func notify(message string) {
+	form := url.Values{
+		"user":    {settings.Pushover["user_key"].(string)},
+		"token":   {settings.Pushover["irc_token"].(string)},
+		"message": {message},
+		"sound":   {"siren"}}
+
+	if resp, err := http.PostForm(settings.Pushover["url"].(string), form); err != nil {
+		log("Failed to send notification: %v", err)
+	} else {
+		defer resp.Body.Close()
+	}
+}
+
+// ...and along comes sexy Mrs. Dash
+func sprinkleMrsDash(data *spicerack.FightCard) {
+	if len(data.MrsDash) > 0 {
+		for _, x := range strings.Split(data.MrsDash, "|") {
+			switch x {
+			case "thats_my_boy":
+				client.Privmsg(settings.Channel, rainbowText("ALL IN ON MR. BONEGOLEM'S WILD RIDE"))
+			}
+		}
+	}
+}
+
+func rainbowText(text string) string {
+	colors := []int{4, 7, 8, 9, 11, 12, 6}
+	buf := bytes.NewBuffer(nil)
+	skip := 0
+	for i, r := range text {
+		if r == ' ' {
+			skip++
+			buf.WriteString(" ")
+		} else {
+			buf.WriteString(fmt.Sprintf("\x03%02d%s", colors[(i-skip)%(len(colors))], string(r)))
+		}
+	}
+	buf.WriteByte(15)
+	return buf.String()
+}
